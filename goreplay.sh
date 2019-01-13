30a19a96e6c8a983a7351c1dfb2e79e2494987d6
dalongrong <1141591465@qq.com>
2019-01-13 12:35:47 +0800

    add es stora demo

--- goreplay.sh
+++ goreplay.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-nohup goreplay --input-raw :80 --output-http "http://app"  --output-http "http://app2" &
+nohup goreplay --input-raw :80 --output-http "http://app"  --output-http "http://app2"  --output-http-elasticsearch es:9200/gor &
